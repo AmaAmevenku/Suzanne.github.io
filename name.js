cb2e772cab6f796b60224f935a5dcdd61830b122
AmaAmevenku <105345783+AmaAmevenku@users.noreply.github.com>
2022-06-23 01:45:48 +0000

    Add files via upload

---
+++ name.js
@@ -0,0 +1,9 @@
+let name= 'Suzanne Amevenku'
+let height = '1.75m'
+let country = 'Ghana'
+
+alert ('Suzanne Amevenku' + '\n' +'1.75m' + '\n' + 'Ghana');
+
+console.log('Suzanne Amevenku')
+console.log('1.75m')
+console.log('Ghana')
